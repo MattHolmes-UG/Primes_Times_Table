c3aa726d59ac82f47f9297aa05b33ff26e79953f
MattHolmes <matthewochonogorug@gmail.com>
2020-01-17 14:31:01 +0100

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,47 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'primegen'`, () => {
+  it(`should have as title Prime Times Tables`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('primegen');
+    expect(app.title).toEqual('Prime Times Tables');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('primegen app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Prime Times Tables');
   });
+
+  it('should have an array of primes equal to the number passed', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    const num = 10;
+    app.showTable(num);
+    expect(app.primes.length).toEqual(num);
+  })
+  it('should have primes with correct values', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    const num = 10;
+    const firstTenPrimes = [2,3,5,7,11,13,17,19,23,29]
+    app.generatePrimes(num);
+    expect(app.primes.length).toEqual(num);
+    expect(app.primes).toEqual(firstTenPrimes);
+    app.generatePrimes(3);
+    expect(app.primes).toEqual(firstTenPrimes.splice(0,3));
+  })
+  it('should have times table array with length equal to number passed', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    const num = 10;
+    app.showTable(num);
+    expect(app.tableArr.length).toEqual(num);
+  })
+  it('should have a table element', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('table')).toBeTruthy();
+  })
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,92 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'primegen';
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('table', { static: true }) table: ElementRef;
+  @ViewChild('num', { static: false }) num: ElementRef;
+  @ViewChild('numbtn', { static: true }) btn: ElementRef;
+  title = 'Prime Times Tables';
+  public primes;
+  public tableArr;
+
+  ngOnInit() {
+    this.showTable(10);
+    this.btn.nativeElement.addEventListener('click', () => {
+      this.showTable(this.num.nativeElement.value)
+    })
+  }
+
+  ngAfterViewInit() {
+
+  }
+
+  generatePrimes = (num: number) => {
+    let primeArr = [],
+      divisors = [2, 3, 5]
+    if (num <= divisors.length) {
+      primeArr = divisors.splice(0, num);
+      // console.log(primeArr);
+      this.primes = primeArr;
+      return this.timesTable(this.primes);
+    }
+    primeArr = divisors;
+
+    let i = primeArr.length;
+    let x = primeArr[i - 1];
+    for (; i < num;) {
+      if (divisors.some(d => { return x % d === 0 })) {
+        x += 1;
+      } else {
+        primeArr.push(x);
+        i += 1; x += 1;
+      }
+    }
+    this.primes = primeArr;
+    // console.log('primes', this.primes);
+    this.timesTable(this.primes)
+
+  }
+  timesTable = (primes: []) => {
+    const tableArr = [];
+    primes.forEach(q => {
+      tableArr.push(primes.map(p => q * p));
+    });
+    this.tableArr = tableArr;
+    console.log(tableArr);
+  }
+  showTable = (num) => {
+    this.generatePrimes(num);
+    this.table.nativeElement.innerHTML = ''; //clear the table
+    //To fill the first row with the primes from with table is gotten
+    let firstRow = document.createElement('tr');
+    firstRow.appendChild(document.createElement('td')); //to add the empty space
+    this.primes.forEach(primenum => {
+      let tdElem = document.createElement('td');
+      tdElem.textContent = primenum;
+      firstRow.appendChild(tdElem);
+
+    });
+    this.table.nativeElement.appendChild(firstRow); //add the first row to the column
+
+    this.tableArr.forEach((row, index) => {
+      let trElem = document.createElement('tr');
+      // console.log(index);
+
+      let firstTdElem = document.createElement('td');
+      firstTdElem.textContent = this.primes[index];
+      trElem.appendChild(firstTdElem);
+      //To fill the table with values
+      row.forEach((primenum) => {
+        //add the value of the primes for that row
+        let tdElem = document.createElement('td');
+        tdElem.textContent = primenum;
+        trElem.appendChild(tdElem);
+      });
+      this.table.nativeElement.appendChild(trElem);
+    });
+  }
 }
